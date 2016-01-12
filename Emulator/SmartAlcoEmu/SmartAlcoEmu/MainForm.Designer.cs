4ebc99b756f7b0c33f48d3f9be5c72872081c54b
Maksim Kharaneka <maksim_kharaneka@epam.com>
2016-01-12 13:32:42 +0300

    Draft serial communication

---
+++ Emulator/SmartAlcoEmu/SmartAlcoEmu/MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SmartAlcoEmu
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.textBoxCom = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(118, 12);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(199, 12);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(75, 23);
+            this.disconnectButton.TabIndex = 1;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // textBoxCom
+            // 
+            this.textBoxCom.Location = new System.Drawing.Point(12, 12);
+            this.textBoxCom.Name = "textBoxCom";
+            this.textBoxCom.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCom.TabIndex = 2;
+            this.textBoxCom.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxCom_KeyPress);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBoxCom);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.connectButton);
+            this.Name = "MainForm";
+            this.Text = "SmartAlcoEmu";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.TextBox textBoxCom;
+    }
+}
+
---
+++ Emulator/SmartAlcoEmu/SmartAlcoEmu/MainForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartAlcoEmu
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Add this variable
+
+        string RxString;
+
+        private void DisplayText(object sender, EventArgs e)
+        {
+            textBoxCom.AppendText(RxString);
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            serialPort.PortName = "COM2";
+            serialPort.BaudRate = 9600;
+
+            serialPort.Open();
+            if (serialPort.IsOpen)
+            {
+                connectButton.Enabled = false;
+                disconnectButton.Enabled = true;
+                textBoxCom.ReadOnly = false;
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+                connectButton.Enabled = true;
+                disconnectButton.Enabled = false;
+                textBoxCom.ReadOnly = true;
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen) serialPort.Close();
+        }
+
+        private void textBoxCom_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // If the port is closed, don't try to send a character.
+
+            if (!serialPort.IsOpen) return;
+
+            // If the port is Open, declare a char[] array with one element.
+            char[] buff = new char[1];
+
+            // Load element 0 with the key character.
+
+            buff[0] = e.KeyChar;
+
+            // Send the one character buffer.
+            serialPort.Write(buff, 0, 1);
+
+            // Set the KeyPress event as handled so the character won't
+            // display locally. If you want it to display, omit the next line.
+            e.Handled = true;
+        }
+
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            RxString = serialPort.ReadExisting();
+            this.Invoke(new EventHandler(DisplayText));
+        }
+    }
+}
